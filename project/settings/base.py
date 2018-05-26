7e6aecf836223c2e3b3c5ff0835f12802e2937d3
oswaldo rodriguez <ioswxd@gmail.com>
2018-05-26 16:40:10 -0500

    Update base.py

--- project/settings/base.py
+++ project/settings/base.py
@@ -115,6 +115,8 @@ TEMPLATES = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ),
 }
